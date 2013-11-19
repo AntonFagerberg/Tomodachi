b1711637b0a682e8dc0a2b790633c53b5bca0d7b
Anton Fagerberg <anton@antonfagerberg.com>
2013-11-19 14:44:11 +0100

    Data is now included in evolutions file instead of h2 db file.

---
+++ conf/evolutions/default/3.sql
@@ -0,0 +1,18 @@
+# --- !Ups
+DELETE FROM category;
+INSERT INTO `category` (`id`, `title`)
+  VALUES
+  (1,'Greetings'),
+  (2,'Verbs'),
+  (3,'Entertainment & Sports'),
+  (4,'Food & Drinks'),
+  (5,'Places'),
+  (6,'Time (text)'),
+  (7,'Adjectives'),
+  (8,'Adverbs'),
+  (9,'Expressions'),
+  (10,'Numbers'),
+  (11,'Kanji');
+
+# --- !Downs
+DELETE FROM category;
