095e6eaf2fcdd86b12c267dbded10cdbec210504
Anton Fagerberg <anton@antonfagerberg.com>
2013-11-21 16:20:08 +0100

    More words.

--- conf/evolutions/default/3.sql
+++ conf/evolutions/default/3.sql
@@ -2,12 +2,12 @@
 DELETE FROM category;
 INSERT INTO `category` (`id`, `title`)
   VALUES
-  (1,'Greetings'),
+  (1,'Things'),
   (2,'Verbs'),
   (3,'Entertainment & Sports'),
   (4,'Food & Drinks'),
   (5,'Places'),
-  (6,'Time (text)'),
+  (6,'Time expressions'),
   (7,'Adjectives'),
   (8,'Adverbs'),
   (9,'Expressions'),
